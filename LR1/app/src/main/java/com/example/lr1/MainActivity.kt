2b52e28a3f9c173c228f579ceeb59f55161beb45
ShadrinaM <143996514+ShadrinaM@users.noreply.github.com>
2025-02-12 22:23:58 +0300

    лр1

---
+++ LR1/app/src/main/java/com/example/lr1/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.lr1
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SuspiciousIndentation")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btn = findViewById<Button>(R.id.btn1)
+        btn.setOnClickListener{
+            val et = findViewById<EditText>(R.id.editTextt)
+            val s = et.text.toString()
+            if (s.isBlank())
+                et.error = "Ничего не введено"
+                return@setOnClickListener
+            findViewById<TextView>(R.id.last_text).text=s
+        }
+    }
+}

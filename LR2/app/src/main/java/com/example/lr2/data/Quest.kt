0c5463d76709bc77e30b40556e8c3be0875b3851
BusinaLapusina <busina_lapusina@mail.ru>
2025-02-13 01:15:17 +0300

    fff

--- LR2/app/src/main/java/com/example/lr2/data/Quest.kt
+++ LR2/app/src/main/java/com/example/lr2/data/Quest.kt
@@ -3,3 +3,4 @@ package com.example.lr2.data
 import androidx.annotation.StringRes
 
 data class Quest(@StringRes val textResId: Int, val answer : Boolean)
+// для хранения данных и автоматически предоставляет методы toString(), equals(), hashCode() и copy()

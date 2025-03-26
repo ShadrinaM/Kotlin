318e35f85ae69cd5a9301bedf97e77e690f7f57b
ShadrinaM <143996514+ShadrinaM@users.noreply.github.com>
2025-03-26 11:42:09 +0300

    updateFaculty

--- LR3/app/src/main/java/com/example/lr3/repositores/OffineDBRepository.kt
+++ LR3/app/src/main/java/com/example/lr3/repositores/OffineDBRepository.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.flow.Flow
 class OffineDBRepository(val dao: MyDAO) : DBRepository {
     override fun getFaculty(): Flow<List<Faculty>> = dao.getFaculty()
     override suspend fun insertFaculty(faculty: Faculty) = dao.insertFaculty(faculty)
-    override suspend fun updateFacylty(faculty: Faculty) = dao.updateFaculty(faculty)
+    override suspend fun updateFaculty(faculty: Faculty) = dao.updateFaculty(faculty)
     override suspend fun insertAllFaculty(facultyList: List<Faculty>) =
         dao.insertAllFaculty(facultyList)
 

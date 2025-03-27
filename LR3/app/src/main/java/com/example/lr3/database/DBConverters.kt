6a8028c2e781b84d6d731877501ff9d19cfae3b7
ShadrinaM <143996514+ShadrinaM@users.noreply.github.com>
2025-03-27 10:47:03 +0300

    две новые таблицы и конвентаторы к ней

--- LR3/app/src/main/java/com/example/lr3/database/DBConverters.kt
+++ LR3/app/src/main/java/com/example/lr3/database/DBConverters.kt
@@ -1,6 +1,7 @@
 package com.example.lr3.database
 
 import androidx.room.TypeConverter
+import java.util.Date
 import java.util.UUID
 
 class DBConverters {
@@ -13,5 +14,18 @@ class DBConverters {
     @TypeConverter
     fun fromUUID(uuid: UUID?): String? {
         return uuid?.toString()
+    }
+
+
+    @TypeConverter
+    fun fromDate(date: Date?): Long? {
+        return date?.time
+    }
+
+    @TypeConverter
+    fun toDate(millisSinceEpoch: Long?): Date? {
+        return millisSinceEpoch?.let{
+            Date(it)
+        }
     }
 }

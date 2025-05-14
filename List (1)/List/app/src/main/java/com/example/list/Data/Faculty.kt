492ce4451906f46e1d4dc902befdf5bb52d2dd1b
ShadrinaM <143996514+ShadrinaM@users.noreply.github.com>
2025-05-14 21:50:59 +0300

    ф

---
+++ List (1)/List/app/src/main/java/com/example/list/Data/Faculty.kt
@@ -0,0 +1,17 @@
+package com.example.list.Data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity (
+    indices = [Index("id")]
+)
+
+data class Faculty (
+    @PrimaryKey val id: UUID = UUID.randomUUID(),
+    @ColumnInfo(name = "faculty_name") var name : String = ""
+
+)
---
+++ List (1)/List/app/src/main/java/com/example/list/Data/MyConsts.kt
@@ -0,0 +1,9 @@
+package com.example.list.Data
+
+class MyConsts {
+    val TAG = "com.example.List.log"
+}
+
+enum class NamesOfFragment {
+    FACULTY, GROUP, STUDENT
+}
